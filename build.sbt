9bc73e09e3e6767f48e7f53386d298a296d30126
merlijn <vanittersum@gmail.com>
2024-07-20 17:21:46 +0200

    fix telegram bot

--- build.sbt
+++ build.sbt
@@ -1,10 +1,8 @@
 import sbt.project
 
-val scala_2_13_version = "2.13.4"
-val scala_3_version = "3.4.2"
-
 val commonSettings = Seq(
   organization := "com.github.merlijn",
+  scalaVersion := "3.4.2",
 )
 
 val circeVersion = "0.14.9"
@@ -14,32 +12,32 @@ lazy val api = project
   .settings(commonSettings)
   .settings(
     name := "llm-api",
-    scalaVersion := scala_3_version,
+
     libraryDependencies ++= Seq(
       "org.slf4j" % "slf4j-api" % "2.0.12",
       "com.softwaremill.sttp.client3" %% "core" % "3.9.7",
       "io.circe"   %% "circe-core"     % circeVersion,
-       "io.circe"  %% "circe-generic"  % circeVersion,
-       "io.circe"  %% "circe-parser"   % circeVersion,
-//      "com.github.andyglow" %% "scala-jsonschema-core" % "0.7.11" cross CrossVersion.for3Use2_13,
-//      "com.github.andyglow" %% "scala-jsonschema-circe-json" % "0.7.11" cross CrossVersion.for3Use2_13,
-      "org.typelevel" %% "cats-core" % "2.12.0",
+      "io.circe"  %% "circe-generic"   % circeVersion,
+      "io.circe"  %% "circe-parser"    % circeVersion,
+      "org.typelevel" %% "cats-core"   % "2.12.0",
     )
   )
 
-//lazy val telegramBot = project
-//  .in(file("examples/telegram-bot"))
-//  .settings(commonSettings)
-//  .settings(
-//    name := "llm-api-example-telegram-bot",
-//    version := "0.1.0-SNAPSHOT",
-//    scalaVersion := scala_2_13_version,
-//    libraryDependencies ++= Seq(
+lazy val telegramBot = project
+  .in(file("examples/telegram-bot"))
+  .settings(commonSettings)
+  .settings(
+    name := "llm-api-example-telegram-bot",
+    version := "0.1.0-SNAPSHOT",
+    libraryDependencies ++= Seq(
 //      "com.bot4s" %% "telegram-core" % "5.8.3" cross CrossVersion.for3Use2_13,
-//      "com.softwaremill.sttp.client3" %% "async-http-client-backend-future" % "3.9.7",
-//      "ch.qos.logback" % "logback-classic" % "1.5.6",
-//    )
-//  ).dependsOn(api)
+      "io.github.apimorphism" %% "telegramium-core" % "9.77.0",
+      "io.github.apimorphism" %% "telegramium-high" % "9.77.0",
+      "com.softwaremill.sttp.client3" %% "cats" % "3.9.7",
+      "com.softwaremill.sttp.client3" %% "async-http-client-backend-future" % "3.9.7",
+      "ch.qos.logback" % "logback-classic" % "1.5.6",
+    )
+  ).dependsOn(api)
 
 lazy val simpleChat = project
   .in(file("examples/simple-chat"))
@@ -47,7 +45,6 @@ lazy val simpleChat = project
   .settings(
     name := "llm-api-example-simple-chat",
     version := "0.1.0-SNAPSHOT",
-    scalaVersion := scala_3_version,
     libraryDependencies ++= Seq(
       "ch.qos.logback" % "logback-classic" % "1.5.6",
       "com.softwaremill.sttp.client3" %% "async-http-client-backend-future" % "3.9.7",
@@ -59,7 +56,6 @@ lazy val functionCall = project
   .settings(commonSettings)
   .settings(
     name := "example-function-call",
-    scalaVersion := scala_3_version,
     version := "0.1.0-SNAPSHOT",
     libraryDependencies ++= Seq(
       "ch.qos.logback" % "logback-classic" % "1.5.6",
--- examples/telegram-bot/src/main/scala/com/github/merlijn/llm/examples/telegram_bot/ExampleTelegramBot.scala
+++ examples/telegram-bot/src/main/scala/com/github/merlijn/llm/examples/telegram_bot/ExampleTelegramBot.scala
@@ -1,40 +1,90 @@
 package com.github.merlijn.llm.examples.telegram_bot
 
-import cats.instances.future._
-import com.github.merlijn.llm.api.OpenAiClient
+import cats.{Monad, Parallel}
+import cats.syntax.all.*
+import cats.effect.{Async, IO}
+import cats.effect.instances.*
+import cats.effect.unsafe.IORuntime
+import cats.instances.future.*
+import com.github.merlijn.llm.api.{OpenAiClient, dto}
+import org.http4s.blaze.client.BlazeClientBuilder
 import sttp.client3.HttpClientFutureBackend
+import sttp.client3.httpclient.cats.HttpClientCatsBackend
 import sttp.model.Uri
+import telegramium.bots.{ChatIntId, Message}
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import telegramium.bots.high.*
+import telegramium.bots.high.implicits.*
 
-object ExampleTelegramBot extends App {
+object ExampleTelegramBot {
 
-  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+  @main
+  def run(): Unit = {
+    given ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+    given ioRuntime: IORuntime = cats.effect.unsafe.IORuntime.global
 
+    val logger = org.slf4j.LoggerFactory.getLogger(getClass)
+
+    val llmToken = sys.env.get("LLM_TOKEN")
+    val llmBaseUrl = sys.env.getOrElse("LLM_BASE_URL", "https://api.openai.com/v1")
+    val llmModel = sys.env.getOrElse("LLM_MODEL", "gpt-3.5-turbo")
+    val telegramToken = sys.env.getOrElse("TELEGRAM_TOKEN", throw new IllegalStateException("TELEGRAM_TOKEN env variable not set"))
+    
+    val botResource = for {
+      openAiClientBackend <- HttpClientCatsBackend.resource[IO]()
+      openAiClient = new OpenAiClient(
+        apiToken = llmToken,
+        backend = openAiClientBackend,
+        baseUri = Uri.parse(llmBaseUrl).getOrElse(throw new IllegalStateException("Invalid base URL"))
+      )
+      telegramBackend <- BlazeClientBuilder[IO].resource
+    } yield {
+      val api: Api[IO] = BotApi(telegramBackend, baseUrl = s"https://api.telegram.org/bot$telegramToken")
+      new TelegramLLMBot(api, openAiClient, llmModel)
+    }
+    
+    botResource.use { bot =>
+      bot.start()
+    }.unsafeRunSync()
+  }
+}
+
+class TelegramLLMBot[F[_]: Async: Parallel : Monad](
+    api: Api[F], 
+    llmClient: OpenAiClient[F],
+    llmModel: String)(using ec: ExecutionContext) extends LongPollBot[F](api) {
+  
   val logger = org.slf4j.LoggerFactory.getLogger(getClass)
-
-  val llmToken = sys.env.get("LLM_TOKEN")
-  val llmBaseUrl = sys.env.getOrElse("LLM_BASE_URL", "https://api.openai.com/v1")
-  val llmModel = sys.env.getOrElse("LLM_MODEL", "gpt-3.5-turbo")
-  val telegramToken = sys.env.getOrElse("TELEGRAM_TOKEN", throw new IllegalStateException("TELEGRAM_TOKEN env variable not set"))
-
-  val openAiClient = new OpenAiClient(
-    apiToken = llmToken,
-    backend = HttpClientFutureBackend(),
-    baseUri = Uri.parse(llmBaseUrl).getOrElse(throw new IllegalStateException("Invalid base URL"))
-  )
-
-  val bot = new SimpleBot(
-    telegramToken = telegramToken,
-    llmClient = openAiClient,
-    llmModel = llmModel
-  )
-
-  val eol = bot.run()
-  scala.io.StdIn.readLine()
-  bot.shutdown() // initiate shutdown
-
-  // Wait for the bot end-of-life
-  Await.result(eol, Duration.Inf)
+  
+  override def onMessage(msg: Message): F[Unit] = {
+    
+    val chatRequest = dto.ChatCompletionRequest(
+      model = llmModel,
+      messages = List(
+        dto.Message.system("Answer shortly"),
+        dto.Message.user(msg.text.getOrElse("")),
+      ),
+      max_tokens = Some(1500),
+      temperature = Some(0.8),
+    )
+    
+    Monad[F].flatMap(llmClient.chatCompletion(chatRequest)) {
+      case Right(response) =>
+        response.firstMessageContent match {
+          case None =>
+            api.execute(Methods.sendMessage(chatId = ChatIntId(msg.chat.id), text = "Failed to get response from LLM.")).void
+          case Some(content) =>
+            api.execute(Methods.sendMessage(chatId = ChatIntId(msg.chat.id), text = content)).void
+        }
+      case Left(error) =>
+        logger.error(s"Failed to send message to LLM or to send response to user: ${error}")
+        api.execute(Methods.sendMessage(chatId = ChatIntId(msg.chat.id), text = "An error occurred while processing your request. Please try again later.")).void
+    }.recover {
+      case ex: Exception =>
+        logger.error(s"Failed to send message to LLM or to send response to user: ${ex.getMessage}")
+        api.execute(Methods.sendMessage(chatId = ChatIntId(msg.chat.id), text = "An error occurred while processing your request. Please try again later.")).void
+    }
+  }
 }
